a5851d56a6ae3a737cf48909c4cf0ec58a77b4aa
VastCosmic <137017677@qq.com>
2023-11-09 20:21:31 +0800

    fix bug

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-# 启动Server
+# 获取文件目录，并启动Server
 import os 
-os.system("python Server\StartServer.py")
+# 获取当前文件目录
+path = os.path.abspath(os.path.dirname(__file__))
+print(path)
+# 启动Server
+os.system("python " + path + "\Server\StartServer.py")
+# 等待输入
+input("Press Enter to continue...")
